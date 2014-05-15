4ce8e90f70d6566299751adf94ca53feb4fa54c2
eberlid <eberlid@gmail.com>
2014-05-16 00:30:43 +0200

    added missing file

---
+++ ShootingRange.BusinessObjects/Session.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ShootingRange.BusinessObjects
+{
+  public class Session
+  {
+    public int SessionId { get; set; }
+    public int ShooterId { get; set; }
+    public int LaneNumber { get; set; }
+    public int ProgramItemId { get; set; }
+    public DateTime Timestamp { get; set; }
+  }
+}
