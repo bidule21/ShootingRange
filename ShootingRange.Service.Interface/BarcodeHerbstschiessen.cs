db45a43365d4325dd9d5dd92a82d243a44d18e0d
eberlid <eberlid@gmail.com>
2015-05-14 11:18:54 +0200

    Add missing files

---
+++ ShootingRange.Service.Interface/BarcodeHerbstschiessen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ShootingRange.Service.Interface
+{
+  public class BarcodeFruehlingsschiessen
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public string Barcode { get; set; }
+    public string SieUndEr { get; set; }
+    public string Gruppenstich { get; set; }
+
+    public bool IsGruppenstich { get; set; }
+    public bool IsWorschtUndBrot { get; set; }
+    public bool IsNachwuchsstich { get; set; }
+    public bool IsSieUndEr { get; set; }
+  }
+}
